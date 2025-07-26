942877ea1dbeb749eb525ea06a2000dcfeedf457
Carlos Afonso <carlos.flach15@gmail.com>
2025-07-26 10:24:21 -0300

    feat: more of event emitters in practice

--- event-emitter/app.js
+++ event-emitter/app.js
@@ -5,7 +5,27 @@ class Emitter extends EventEmitter {}
 const myE = new Emitter();
 
 myE.on('foo', () => {
-	console.log('Event ocurred.');
+	console.log('Event ocurred 1.');
+});
+
+myE.on('foo', () => {
+	console.log('Event ocurred 2.');
+});
+
+myE.on('foo', x => {
+	console.log('An event with parameter occurred.');
+	console.log(x);
+});
+
+myE.once('bar', () => {
+	console.log('An event occurred bar');
 });
 
 myE.emit('foo');
+myE.emit('foo', 'some text');
+
+myE.emit('bar');
+myE.emit('bar');
+myE.emit('bar');
+myE.emit('bar');
+myE.emit('bar');
